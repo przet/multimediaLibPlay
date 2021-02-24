5faad663725afa79f66ff5394fa3601f44969f90
Philippe Rzetelski <p.rzetelski.note@gmail.com>
2021-02-25 10:05:09 +1030

    Have window in middle of screen

--- graphics/opengl/Initial/Source.cpp
+++ graphics/opengl/Initial/Source.cpp
@@ -70,10 +70,14 @@ void init()
 
 int main(int argc, char** argv)
 {
+	float width = 3840;
+	float height = 2160;
+	float factor = 0.3;
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowSize(250, 250);
-	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(width*(factor), height*(factor));
+	glutInitWindowPosition(width*(factor), height*(factor));
 	glutCreateWindow("OpenGL Initial");
 	init();
 	glutDisplayFunc(display);
